544ca3f62e2f208e9abe5e412066929358597e77
Bo Zhang <bo@gradle.com>
2019-06-28 11:12:33 +0800

    Sync

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Java面向对象初步练习：将代码重构到抽象的父类骨架中
 
-请完成[`Main`](https://github.com/hcsp/refactor-to-abstract-class/blob/master/src/main/java/com/github/hcsp/polymorphism/Main.java)中的挑战，完成对`西红柿炒鸡蛋`/`清炒菜心`/`煎鸡蛋`几个类的重构
+请完成[`Main`](https://github.com/hcsp/refactor-to-abstract-class/blob/master/src/main/java/com/github/hcsp/polymorphism/)中的挑战，完成对`西红柿炒鸡蛋`/`清炒菜心`/`煎鸡蛋`几个类的重构
 
 > 注：重构指的是在**不改变**原先代码功能的前提下，将原先的代码变得更精炼、可维护性更强。
 
@@ -8,7 +8,7 @@
 
 -----
 注意！我们只允许你修改以下文件，对其他文件的修改会被拒绝：
-- [src/main/java/com/github/hcsp/polymorphism/Main.java](https://github.com/hcsp/refactor-to-abstract-class/blob/master/src/main/java/com/github/hcsp/polymorphism/Main.java)
+- [src/main/java/com/github/hcsp/polymorphism/](https://github.com/hcsp/refactor-to-abstract-class/blob/master/src/main/java/com/github/hcsp/polymorphism/)
 -----
 
 
--- src/test/java/com/github/hcsp/polymorphism/MainTest.java
+++ src/test/java/com/github/hcsp/polymorphism/MainTest.java
@@ -1,7 +1,10 @@
 package com.github.hcsp.polymorphism;
 
 import com.github.blindpirate.extensions.CaptureSystemOutput;
+import com.github.hcsp.test.helper.ProjectSourceFileReader;
+import java.util.stream.Stream;
 import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class MainTest {
@@ -33,5 +36,13 @@ public class MainTest {
                         "(?s).*洗煎锅\\s+倒油\\s+放牛排\\s+煎啊煎啊煎\\s+放胡椒粉\\s放盐\\s+香喷喷的煎牛排出锅啦\\s+.*"));
         菜 一个菜 = new 煎牛排();
         一个菜.做一个菜();
+    }
+
+    @Test
+    public void hasOverries() {
+        Assertions.assertTrue(
+                Stream.of(西红柿炒鸡蛋.class, 清炒菜心.class, 煎牛排.class)
+                        .map(ProjectSourceFileReader::readAsString)
+                        .allMatch(code -> code.contains("@Override")));
     }
 }
