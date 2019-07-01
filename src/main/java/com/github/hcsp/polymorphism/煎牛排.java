0b07ef81e1e1a67f2686852746472955b4bd7e92
Lingfeng <2354833@qq.com>
2019-07-01 13:18:18 +0800

    做菜

--- src/main/java/com/github/hcsp/polymorphism/煎牛排.java
+++ src/main/java/com/github/hcsp/polymorphism/煎牛排.java
@@ -1,14 +1,7 @@
 package com.github.hcsp.polymorphism;
 
 public class 煎牛排 extends 菜 {
-    public void 煎个牛排() {
 
-        System.out.println("倒油");
-
-        System.out.println("放胡椒粉");
-        System.out.println("放盐");
-        System.out.println("香喷喷的煎牛排出锅啦");
-    }
 
     public 煎牛排(){
         super("煎牛排");
