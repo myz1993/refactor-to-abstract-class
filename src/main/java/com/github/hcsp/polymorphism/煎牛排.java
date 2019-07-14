5c99246919ae89719a2ca927ffc5326198630d40
DeeJayStudy <52043001+DeeJayStudy@users.noreply.github.com>
2019-07-14 23:39:59 +0800

    update (#18)
    update (#18)

--- src/main/java/com/github/hcsp/polymorphism/煎牛排.java
+++ src/main/java/com/github/hcsp/polymorphism/煎牛排.java
@@ -1,13 +1,26 @@
 package com.github.hcsp.polymorphism;
 
-public class 煎牛排 {
-    public void 煎个牛排() {
+public class 煎牛排 extends 菜{
+
+    @Override
+    public void 洗锅() {
         System.out.println("洗煎锅");
-        System.out.println("倒油");
+    }
+
+    @Override
+    void 开始烹饪() {
         System.out.println("放牛排");
         System.out.println("煎啊煎啊煎");
+    }
+
+    @Override
+    public void 放佐料() {
         System.out.println("放胡椒粉");
-        System.out.println("放盐");
+        super.放佐料();
+    }
+
+    @Override
+    void 出锅() {
         System.out.println("香喷喷的煎牛排出锅啦");
     }
 }
