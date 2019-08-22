8d63aebec92b5e14912348149ebfd86812a6202c
haojie <42760638+gakei@users.noreply.github.com>
2019-08-22 12:38:29 -0500

    将代码重构到抽象的父类骨架中 (#38)
    将代码重构到抽象的父类骨架中 (#38)

--- src/main/java/com/github/hcsp/polymorphism/清炒菜心.java
+++ src/main/java/com/github/hcsp/polymorphism/清炒菜心.java
@@ -1,7 +1,11 @@
 package com.github.hcsp.polymorphism;
 
-public class 清炒菜心 {
-    public void 做个清炒菜心() {
+public class 清炒菜心 extends 菜{
+    public 清炒菜心() {
+        super("清炒菜心");
+    }
+
+    /*public void 做个清炒菜心() {
         System.out.println("洗炒锅");
         System.out.println("倒一点点油");
         System.out.println("放青菜");
@@ -9,5 +13,27 @@ public class 清炒菜心 {
         System.out.println("放酱油");
         System.out.println("放盐");
         System.out.println("香喷喷的清炒菜心出锅啦");
+    }*/
+
+    @Override
+    public void 洗锅() {
+        System.out.println("洗炒锅");
+    }
+
+    @Override
+    public void 倒油() {
+        System.out.println("倒一点点油");
+    }
+
+    @Override
+    public void 开始烹饪() {
+        System.out.println("放青菜");
+        System.out.println("炒啊炒啊炒");
+    }
+
+    @Override
+    public void 放佐料() {
+        System.out.println("放酱油");
+        System.out.println("放盐");
     }
 }
--- src/main/java/com/github/hcsp/polymorphism/菜.java
+++ src/main/java/com/github/hcsp/polymorphism/菜.java
@@ -1,21 +1,31 @@
 package com.github.hcsp.polymorphism;
 
 public class 菜 {
+    private String 菜名;
+    菜(String 菜名){
+        this.菜名 = 菜名;
+    }
     public void 做一个菜() {
         洗锅();
         倒油();
         开始烹饪();
         放佐料();
-        出锅();
+        出锅(菜名);
     }
 
-    public void 洗锅() {}
+    public void 洗锅() {
+        System.out.println("洗锅");
+    }
 
-    public void 倒油() {}
+    public void 倒油() {
+        System.out.println("倒油");
+    }
 
     public void 开始烹饪() {}
 
     public void 放佐料() {}
 
-    public void 出锅() {}
+    public void 出锅(String 菜名) {
+        System.out.println("香喷喷的" + 菜名 + "出锅啦");
+    }
 }
