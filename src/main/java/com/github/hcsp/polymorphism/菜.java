6253a1200a0308c073fd3c8485639cfd6d411801
EthanWXL <46627960+EthanWXL@users.noreply.github.com>
2019-08-25 16:40:49 +0800

    将代码重构到抽象的父类骨架中 (#39)
    将代码重构到抽象的父类骨架中 (#39)

--- src/main/java/com/github/hcsp/polymorphism/菜.java
+++ src/main/java/com/github/hcsp/polymorphism/菜.java
@@ -9,13 +9,23 @@ public class 菜 {
         出锅();
     }
 
-    public void 洗锅() {}
+    public void 洗锅() {
+        System.out.println("洗炒锅");
+    }
 
-    public void 倒油() {}
+    public void 倒油() {
+        System.out.println("倒油");
+    }
 
-    public void 开始烹饪() {}
+    public void 开始烹饪() {
+        System.out.println("炒啊炒啊炒");
+    }
 
-    public void 放佐料() {}
+    public void 放佐料() {
+        System.out.println("放盐");
+    }
 
-    public void 出锅() {}
+    public void 出锅() {
+        System.out.println("出锅");
+    }
 }
