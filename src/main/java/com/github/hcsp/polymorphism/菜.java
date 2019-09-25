55644f95a4e19c84e5186dae74ddfcdb288a3843
OrangeC <38210165+903875816@users.noreply.github.com>
2019-09-25 21:59:58 +0800

    将代码重构到抽象的父类骨架中 (#53)
    将代码重构到抽象的父类骨架中 (#53)

--- src/main/java/com/github/hcsp/polymorphism/菜.java
+++ src/main/java/com/github/hcsp/polymorphism/菜.java
@@ -1,6 +1,6 @@
 package com.github.hcsp.polymorphism;
 
-public class 菜 {
+public abstract class 菜 {
     public void 做一个菜() {
         洗锅();
         倒油();
@@ -9,13 +9,28 @@ public class 菜 {
         出锅();
     }
 
-    public void 洗锅() {}
+    public void 洗锅() {
+        String name = this.getClass().getSimpleName();
+        if(name.contains("煎")){
+            System.out.println("洗煎锅");
+        }else if(name.contains("炒")){
+            System.out.println("洗炒锅");
+        }else{
+            System.out.println("生成错误,请输入你要使用的工具:");
+        }
+    }
 
-    public void 倒油() {}
+    public void 倒油() {
+        System.out.println("倒油");
+    }
 
-    public void 开始烹饪() {}
+    public abstract void 开始烹饪();
 
-    public void 放佐料() {}
+    public void 放佐料() {
+        System.out.println("放盐");
+    }
 
-    public void 出锅() {}
+    public void 出锅() {
+        System.out.println("香喷喷的"+ this.getClass().getSimpleName()+"出锅啦");
+    }
 }
