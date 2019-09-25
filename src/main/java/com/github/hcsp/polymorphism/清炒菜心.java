e8b47a1c5df23147adc3aee84582b613a2ab9ab7
JiaXinWang61 <584937529@qq.com>
2019-09-25 22:04:41 +0800

    将代码重构到抽象的父类骨架中

--- src/main/java/com/github/hcsp/polymorphism/清炒菜心.java
+++ src/main/java/com/github/hcsp/polymorphism/清炒菜心.java
@@ -1,7 +1,27 @@
 package com.github.hcsp.polymorphism;
 
-public class 清炒菜心 {
-    public void 做个清炒菜心() {
+public class 清炒菜心 extends 菜 {
+
+    @Override
+    public void 倒油() {
+        System.out.println("倒一点点油");
+    }
+
+    @Override
+    public void 开始烹饪() {
+        System.out.println("放青菜");
+        super.开始烹饪();
+    }
+
+    @Override
+    public void 放佐料() {
+        System.out.println("放酱油");
+        super.放佐料();
+    }
+    public void 出锅() {
+        System.out.println("香喷喷的清炒菜心出锅了");
+    }
+    /*public void 做个清炒菜心() {
         System.out.println("洗炒锅");
         System.out.println("倒一点点油");
         System.out.println("放青菜");
@@ -9,5 +29,5 @@ public class 清炒菜心 {
         System.out.println("放酱油");
         System.out.println("放盐");
         System.out.println("香喷喷的清炒菜心出锅啦");
-    }
+    }*/
 }
