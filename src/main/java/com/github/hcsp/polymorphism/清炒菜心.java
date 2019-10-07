085fa400cbe7cd18a09f500e8fd2f1ee2c30139a
yueyue chou <13444339+yueyuezone@users.noreply.github.com>
2019-10-07 17:09:04 +0800

    封装 (#60)
    封装 (#60)

--- src/main/java/com/github/hcsp/polymorphism/清炒菜心.java
+++ src/main/java/com/github/hcsp/polymorphism/清炒菜心.java
@@ -1,6 +1,28 @@
 package com.github.hcsp.polymorphism;
 
-public class 清炒菜心 {
+public class 清炒菜心 extends 菜{
+    @Override
+    public void 倒油() {
+        System.out.println("倒一点点油");
+    }
+
+    @Override
+    public void 开始烹饪() {
+        System.out.println("放青菜");
+        super.开始烹饪();
+    }
+
+    @Override
+    public void 放佐料() {
+        System.out.println("放酱油");
+        super.放佐料();
+    }
+
+    @Override
+    public void 出锅() {
+        System.out.println("香喷喷的清炒菜心出锅啦");
+    }
+
     public void 做个清炒菜心() {
         System.out.println("洗炒锅");
         System.out.println("倒一点点油");
--- src/main/java/com/github/hcsp/polymorphism/西红柿炒鸡蛋.java
+++ src/main/java/com/github/hcsp/polymorphism/西红柿炒鸡蛋.java
@@ -1,6 +1,18 @@
 package com.github.hcsp.polymorphism;
 
-public class 西红柿炒鸡蛋 {
+public class 西红柿炒鸡蛋 extends 菜{
+    @Override
+    public void 开始烹饪() {
+        System.out.println("放鸡蛋");
+        System.out.println("放西红柿");
+        super.开始烹饪();
+    }
+
+    @Override
+    public void 出锅() {
+        System.out.println("香喷喷的西红柿炒鸡蛋出锅啦");
+    }
+
     public void 做个西红柿炒鸡蛋() {
         System.out.println("洗炒锅");
         System.out.println("倒油");
