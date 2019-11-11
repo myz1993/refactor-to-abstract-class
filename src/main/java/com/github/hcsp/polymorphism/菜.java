68aae5ff695af5aa2b6e7d07b16026a615fe0ae7
JN0916 <153080059@qq.com>
2019-11-11 23:01:26 +0800

    将代码重构到抽象的父类骨架中

--- src/main/java/com/github/hcsp/polymorphism/菜.java
+++ src/main/java/com/github/hcsp/polymorphism/菜.java
@@ -1,6 +1,16 @@
 package com.github.hcsp.polymorphism;
 
 public class 菜 {
+    private String name;
+
+    public 菜(String name) {
+        this.name = name;
+    }
+
+    public 菜() {
+
+    }
+
     public void 做一个菜() {
         洗锅();
         倒油();
@@ -9,13 +19,22 @@ public class 菜 {
         出锅();
     }
 
-    public void 洗锅() {}
+    public void 洗锅() {
+        System.out.println("洗炒锅");
+    }
 
-    public void 倒油() {}
+    public void 倒油() {
+        System.out.println("倒油");
+    }
 
-    public void 开始烹饪() {}
+    public void 开始烹饪() {
+    }
 
-    public void 放佐料() {}
+    public void 放佐料() {
+        System.out.println("放盐");
+    }
 
-    public void 出锅() {}
+    public void 出锅() {
+        System.out.println("香喷喷的" + name + "出锅啦");
+    }
 }
