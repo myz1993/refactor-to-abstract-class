e1c9531d2e51b2838e4e51172ea097e4166cad64
linsinan1995 <47880367+linsinan1995@users.noreply.github.com>
2019-12-24 03:16:38 +0800

    refactor recipes (#90)
    refactor recipes (#90)

--- src/main/java/com/github/hcsp/polymorphism/菜.java
+++ src/main/java/com/github/hcsp/polymorphism/菜.java
@@ -1,6 +1,8 @@
 package com.github.hcsp.polymorphism;
 
 public class 菜 {
+    String name;
+    String cookMethod;
     public void 做一个菜() {
         洗锅();
         倒油();
@@ -9,13 +11,28 @@ public class 菜 {
         出锅();
     }
 
-    public void 洗锅() {}
+    public 菜(String name, String cookMethod) {
+        this.name = name;
+        this.cookMethod = cookMethod;
+    }
 
-    public void 倒油() {}
+    public void 洗锅() {
+        System.out.println("洗"+cookMethod+"锅");
+    }
 
-    public void 开始烹饪() {}
+    public void 倒油() {
+        System.out.println("倒一点点油");
+    }
 
-    public void 放佐料() {}
+    public void 开始烹饪() {
+        System.out.println(cookMethod+"啊"+cookMethod+"啊"+cookMethod);
+    }
 
-    public void 出锅() {}
+    public void 放佐料() {
+        System.out.println("放盐");
+    }
+
+    public void 出锅() {
+        System.out.println("香喷喷的"+name+"出锅啦");
+    }
 }
