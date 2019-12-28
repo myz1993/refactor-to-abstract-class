9164abf7ab08a109c762487aa758c3046091c8c8
JaneWangCoA <43361638+JaneWangCOA@users.noreply.github.com>
2019-12-28 18:25:39 +0800

    使用多态炒菜 (#94)
    使用多态炒菜 (#94)

--- src/main/java/com/github/hcsp/polymorphism/煎牛排.java
+++ src/main/java/com/github/hcsp/polymorphism/煎牛排.java
@@ -1,13 +1,21 @@
 package com.github.hcsp.polymorphism;
 
-public class 煎牛排 {
-    public void 煎个牛排() {
-        System.out.println("洗煎锅");
-        System.out.println("倒油");
-        System.out.println("放牛排");
-        System.out.println("煎啊煎啊煎");
-        System.out.println("放胡椒粉");
-        System.out.println("放盐");
-        System.out.println("香喷喷的煎牛排出锅啦");
-    }
+public class 煎牛排 extends 菜 {
+
+  @Override
+  public void 洗锅() {
+    System.out.println("洗煎锅");
+  }
+
+  @Override
+  public void 开始烹饪() {
+    System.out.println("放牛排");
+    System.out.println("煎啊煎啊煎");
+  }
+
+  @Override
+  public void 放佐料() {
+    System.out.println("放胡椒粉");
+    super.放佐料();
+  }
 }
--- src/main/java/com/github/hcsp/polymorphism/菜.java
+++ src/main/java/com/github/hcsp/polymorphism/菜.java
@@ -1,21 +1,32 @@
 package com.github.hcsp.polymorphism;
 
 public class 菜 {
-    public void 做一个菜() {
-        洗锅();
-        倒油();
-        开始烹饪();
-        放佐料();
-        出锅();
-    }
 
-    public void 洗锅() {}
+  public void 做一个菜() {
+    洗锅();
+    倒油();
+    开始烹饪();
+    放佐料();
+    出锅();
+  }
 
-    public void 倒油() {}
+  public void 洗锅() {
+    System.out.println("洗炒锅");
+  }
 
-    public void 开始烹饪() {}
+  public void 倒油() {
+    System.out.println("倒油");
+  }
 
-    public void 放佐料() {}
+  public void 开始烹饪() {
+    System.out.println("炒啊炒啊炒");
+  }
 
-    public void 出锅() {}
+  public void 放佐料() {
+    System.out.println("放盐");
+  }
+
+  public void 出锅() {
+    System.out.println("香喷喷的" + this.getClass().getSimpleName() + "出锅啦");
+  }
 }
