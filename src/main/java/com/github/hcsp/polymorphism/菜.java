223fc7b06b9454a1cafcdd0fab111d658ae6ac41
zml59 <wan624711573@163.com>
2020-01-15 23:15:10 +0800

    做菜。

--- src/main/java/com/github/hcsp/polymorphism/菜.java
+++ src/main/java/com/github/hcsp/polymorphism/菜.java
@@ -1,6 +1,14 @@
 package com.github.hcsp.polymorphism;
 
 public class 菜 {
+    private String name;
+    private String style;
+
+    public 菜(String name,String style) {
+        this.name = name;
+        this.style=style;
+    }
+
     public void 做一个菜() {
         洗锅();
         倒油();
@@ -9,13 +17,23 @@ public class 菜 {
         出锅();
     }
 
-    public void 洗锅() {}
+    public void 洗锅() {
+        System.out.println("洗"+ style +"锅");
+    }
 
-    public void 倒油() {}
+    public void 倒油() {
+        System.out.println("倒油");
+    }
 
-    public void 开始烹饪() {}
+    public void 开始烹饪() {
+        System.out.println(style+"啊"+style+"啊"+style);
+    }
 
-    public void 放佐料() {}
+    public void 放佐料() {
+        System.out.println("放盐");
+    }
 
-    public void 出锅() {}
+    public void 出锅() {
+        System.out.println("香喷喷的" + name + "出锅啦");
+    }
 }
