c317a8d19e377c30ff2a6e6344dfc31256f144be
YingHuaLiu <35859346+YingHuaLiu@users.noreply.github.com>
2020-02-05 09:18:39 +0800

    继承，多态 (#114)
    继承，多态 (#114)

--- src/main/java/com/github/hcsp/polymorphism/清炒菜心.java
+++ src/main/java/com/github/hcsp/polymorphism/清炒菜心.java
@@ -1,13 +1,24 @@
 package com.github.hcsp.polymorphism;
 
-public class 清炒菜心 {
-    public void 做个清炒菜心() {
-        System.out.println("洗炒锅");
+public class 清炒菜心 extends 菜{
+    public 清炒菜心(){
+        super("清炒菜心");
+    }
+
+    @Override
+    public void 倒油() {
         System.out.println("倒一点点油");
+    }
+
+    @Override
+    public void 开始烹饪() {
         System.out.println("放青菜");
         System.out.println("炒啊炒啊炒");
+    }
+
+    @Override
+    public void 放佐料() {
         System.out.println("放酱油");
         System.out.println("放盐");
-        System.out.println("香喷喷的清炒菜心出锅啦");
     }
 }
