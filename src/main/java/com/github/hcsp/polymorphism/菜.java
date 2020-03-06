5fb986c79ba8333505d7ecfc1119ee713c57942b
硬核机器人 <49554844+hcsp-bot@users.noreply.github.com>
2020-03-06 09:01:31 +0800

    cai (#131)
    cai (#131)

--- src/main/java/com/github/hcsp/polymorphism/菜.java
+++ src/main/java/com/github/hcsp/polymorphism/菜.java
@@ -1,6 +1,7 @@
 package com.github.hcsp.polymorphism;
 
 public class 菜 {
+
     public void 做一个菜() {
         洗锅();
         倒油();
@@ -9,13 +10,21 @@ public class 菜 {
         出锅();
     }
 
-    public void 洗锅() {}
+    public void 洗锅() {
+        System.out.println("洗炒锅");
+    }
 
-    public void 倒油() {}
+    public void 倒油() {
+        System.out.println("倒油");
+    }
 
-    public void 开始烹饪() {}
+    public void 开始烹饪() {
+    }
 
-    public void 放佐料() {}
+    public void 放佐料() {
+        System.out.println("放盐");
+    }
 
-    public void 出锅() {}
+    public void 出锅() {
+    }
 }
