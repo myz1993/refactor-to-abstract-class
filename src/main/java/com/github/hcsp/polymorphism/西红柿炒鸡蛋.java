1d3ae3f978737ffd8f0e9738072be63c1b417626
举高高 <463927289@qq.com>
2020-03-10 15:08:00 +0800

    将代码重构到抽象的父类骨架中 (#134)
    将代码重构到抽象的父类骨架中 (#134)

--- src/main/java/com/github/hcsp/polymorphism/西红柿炒鸡蛋.java
+++ src/main/java/com/github/hcsp/polymorphism/西红柿炒鸡蛋.java
@@ -1,13 +1,45 @@
 package com.github.hcsp.polymorphism;
 
-public class 西红柿炒鸡蛋 {
-    public void 做个西红柿炒鸡蛋() {
+public class 西红柿炒鸡蛋 extends 菜 {
+   /* public void 做个西红柿炒鸡蛋() {
         System.out.println("洗炒锅");
         System.out.println("倒油");
         System.out.println("放鸡蛋");
         System.out.println("放西红柿");
         System.out.println("炒啊炒啊炒");
         System.out.println("放盐");
+        System.out.println("香喷喷的西红柿炒鸡蛋出锅啦");
+    }*/
+
+    @Override
+    public void 做一个菜() {
+        super.做一个菜();
+    }
+
+    @Override
+    public void 洗锅() {
+        System.out.println("洗炒锅");
+    }
+
+    @Override
+    public void 倒油() {
+        System.out.println("倒油");
+    }
+
+    @Override
+    public void 开始烹饪() {
+        System.out.println("放鸡蛋");
+        System.out.println("放西红柿");
+        System.out.println("炒啊炒啊炒");
+    }
+
+    @Override
+    public void 放佐料() {
+        System.out.println("放盐");
+    }
+
+    @Override
+    public void 出锅() {
         System.out.println("香喷喷的西红柿炒鸡蛋出锅啦");
     }
 }
