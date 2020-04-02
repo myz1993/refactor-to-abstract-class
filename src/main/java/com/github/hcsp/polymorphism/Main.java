094a9beeb68b9f20340fef1fb8e4bd68598115a3
wuzhenquan <782754354@qq.com>
2020-04-02 21:01:42 +0800

    将代码重构到抽象的父类骨架中 (#147)
    将代码重构到抽象的父类骨架中 (#147)

--- src/main/java/com/github/hcsp/polymorphism/Main.java
+++ src/main/java/com/github/hcsp/polymorphism/Main.java
@@ -1,7 +1,7 @@
 package com.github.hcsp.polymorphism;
 
-public class Main {
-    // 请仔细观察`西红柿炒鸡蛋`/`清炒菜心`/`煎鸡蛋`几个类
+public class  Main {
+    // 请仔细观察`西红柿炒鸡蛋`/`清炒菜心`/`煎鸡蛋`几                                                                                                                               个类
     // 提取他们的公共代码到`菜`中，完成重构
     // 注意不要修改各个菜的烹饪步骤
     public static void main(String[] args) {
