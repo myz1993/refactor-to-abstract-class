a71d1e39a10649197b6d919e6a0747e8b109ec0a
BrightWhisper <41850676+BrightWhisper@users.noreply.github.com>
2020-04-22 22:39:49 +0800

    将代码重构到抽象的父类骨架中 (#158)
    将代码重构到抽象的父类骨架中 (#158)

--- src/main/java/com/github/hcsp/polymorphism/煎牛排.java
+++ src/main/java/com/github/hcsp/polymorphism/煎牛排.java
@@ -1,13 +1,29 @@
 package com.github.hcsp.polymorphism;
 
-public class 煎牛排 {
-    public void 煎个牛排() {
+public class 煎牛排 extends 菜 {
+
+    @Override
+    public void 洗锅() {
         System.out.println("洗煎锅");
-        System.out.println("倒油");
+    }
+
+    public 煎牛排() {
+        this("煎牛排");
+    }
+
+    public 煎牛排(String name) {
+        super(name);
+    }
+
+    @Override
+    public void 开始烹饪() {
         System.out.println("放牛排");
         System.out.println("煎啊煎啊煎");
+    }
+
+    @Override
+    public void 放佐料() {
         System.out.println("放胡椒粉");
-        System.out.println("放盐");
-        System.out.println("香喷喷的煎牛排出锅啦");
+        super.放佐料();
     }
 }
