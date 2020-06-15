e6fb81e3be567e670cf657007a58faf543f48821
Run Ze Zheng <63222815+runzez-reyes@users.noreply.github.com>
2020-06-15 08:12:34 +0800

    refactor to abstract class (#176)
    refactor to abstract class (#176)

--- src/main/java/com/github/hcsp/polymorphism/Main.java
+++ src/main/java/com/github/hcsp/polymorphism/Main.java
@@ -5,7 +5,7 @@ public class Main {
     // 提取他们的公共代码到`菜`中，完成重构
     // 注意不要修改各个菜的烹饪步骤
     public static void main(String[] args) {
-        菜[] 菜们 = new 菜[] {new 西红柿炒鸡蛋(), new 清炒菜心(), new 煎牛排()};
+        菜[] 菜们 = new 菜[]{new 西红柿炒鸡蛋(), new 清炒菜心(), new 煎牛排()};
         for (菜 一个菜 : 菜们) {
             一个菜.做一个菜();
         }
