23b908728217124390c0ecb45cc828de561fbd4b
Leo <790868516@qq.com>
2020-07-04 16:25:00 +0800

    将代码重构到抽象的父类骨架中

--- src/main/java/com/github/hcsp/polymorphism/煎牛排.java
+++ src/main/java/com/github/hcsp/polymorphism/煎牛排.java
@@ -2,6 +2,11 @@ package com.github.hcsp.polymorphism;
 
 public class 煎牛排 extends 菜 {
     @Override
+    public void 洗锅() {
+        System.out.println("洗煎锅");
+    }
+
+    @Override
     public void 开始烹饪() {
         System.out.println("放牛排");
         System.out.println("煎啊煎啊煎");
