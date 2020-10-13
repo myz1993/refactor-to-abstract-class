023a8401f5232783dfe4f22d59ee1eaa674bf6ab
catmmao <catmmao@gmail.com>
2020-10-13 10:22:14 +0800

    用继承和覆盖 (#191)
    用继承和覆盖 (#191)

--- src/main/java/com/github/hcsp/polymorphism/菜.java
+++ src/main/java/com/github/hcsp/polymorphism/菜.java
@@ -1,6 +1,12 @@
 package com.github.hcsp.polymorphism;
 
 public class 菜 {
+    String name;
+
+    public 菜(String name) {
+        this.name = name;
+    }
+
     public void 做一个菜() {
         洗锅();
         倒油();
@@ -9,13 +15,21 @@ public class 菜 {
         出锅();
     }
 
-    public void 洗锅() {}
+    public void 洗锅() {
+        System.out.println("洗炒锅");
+    }
 
-    public void 倒油() {}
+    public void 倒油() {
+        System.out.println("倒油");
+    }
 
-    public void 开始烹饪() {}
+    public void 开始烹饪() {
+    }
 
-    public void 放佐料() {}
+    public void 放佐料() {
+    }
 
-    public void 出锅() {}
+    public void 出锅() {
+        System.out.println("香喷喷的" + name + "出锅啦");
+    }
 }
