113c5fa5b8cc7340a9af94916131c2876226f45b
yang song <1479867360@qq.com>
2020-12-11 18:34:17 +0800

    将代码重构到抽象的父类骨架中 (#200)
    将代码重构到抽象的父类骨架中 (#200)

--- src/main/java/com/github/hcsp/polymorphism/菜.java
+++ src/main/java/com/github/hcsp/polymorphism/菜.java
@@ -1,6 +1,8 @@
 package com.github.hcsp.polymorphism;
 
 public class 菜 {
+    String dishName;
+
     public void 做一个菜() {
         洗锅();
         倒油();
@@ -9,13 +11,25 @@ public class 菜 {
         出锅();
     }
 
-    public void 洗锅() {}
+    public 菜(String dishName) {
+        this.dishName = dishName;
+    }
 
-    public void 倒油() {}
+    public void 洗锅() {
+        System.out.println("洗炒锅");
+    }
 
-    public void 开始烹饪() {}
+    public void 倒油() {
+        System.out.println("倒油");
+    }
 
-    public void 放佐料() {}
+    public void 开始烹饪() {
+    }
 
-    public void 出锅() {}
+    public void 放佐料() {
+    }
+
+    public void 出锅() {
+        System.out.println("香喷喷的" + dishName + "出锅啦");
+    }
 }
