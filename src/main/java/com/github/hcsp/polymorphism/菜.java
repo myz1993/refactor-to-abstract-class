3dc0168a525564df6c5b850431b0af1e302ab41e
Apolo Du <dudueasy@126.com>
2021-02-10 16:49:55 +0800

    update (#207)
    update (#207)

--- src/main/java/com/github/hcsp/polymorphism/菜.java
+++ src/main/java/com/github/hcsp/polymorphism/菜.java
@@ -1,6 +1,12 @@
 package com.github.hcsp.polymorphism;
 
 public class 菜 {
+    final private String 菜名;
+
+    public 菜(String 菜名) {
+        this.菜名 = 菜名;
+    }
+
     public void 做一个菜() {
         洗锅();
         倒油();
@@ -9,13 +15,18 @@ public class 菜 {
         出锅();
     }
 
-    public void 洗锅() {}
+    public void 洗锅() {
+    }
 
-    public void 倒油() {}
+    public void 倒油() {
+    }
 
     public void 开始烹饪() {}
 
-    public void 放佐料() {}
+    public void 放佐料() {
+    }
 
-    public void 出锅() {}
+    public void 出锅() {
+        System.out.println("香喷喷的"+ 菜名 +"出锅啦");
+    }
 }
