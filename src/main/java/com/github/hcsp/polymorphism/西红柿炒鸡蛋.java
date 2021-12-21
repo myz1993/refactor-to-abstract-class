a18e6cdeaa51e9744a645a743af35eea4e7a58c1
jcbound.ma <46434117+jcbound@users.noreply.github.com>
2021-12-21 23:27:58 +0800

    优化代码结构，完成任务 (#236)
    优化代码结构，完成任务 (#236)

--- src/main/java/com/github/hcsp/polymorphism/西红柿炒鸡蛋.java
+++ src/main/java/com/github/hcsp/polymorphism/西红柿炒鸡蛋.java
@@ -1,13 +1,21 @@
 package com.github.hcsp.polymorphism;
 
-public class 西红柿炒鸡蛋 {
+public class 西红柿炒鸡蛋 extends 菜 {
+    public 西红柿炒鸡蛋() {
+        super("西红柿炒鸡蛋");
+    }
+
     public void 做个西红柿炒鸡蛋() {
-        System.out.println("洗炒锅");
-        System.out.println("倒油");
+
+
+
+
+
+    }
+    @Override
+    public void 开始烹饪() {
         System.out.println("放鸡蛋");
         System.out.println("放西红柿");
         System.out.println("炒啊炒啊炒");
-        System.out.println("放盐");
-        System.out.println("香喷喷的西红柿炒鸡蛋出锅啦");
     }
 }
